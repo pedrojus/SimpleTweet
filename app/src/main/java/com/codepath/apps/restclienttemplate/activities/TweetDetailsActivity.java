fad48ff23ee2caf5c8806c2b08b71ea4422dd3a6
pedrojus <p.jusino@hotmail.com>
2021-10-03 20:02:45 -0500

    added logo to activity

--- app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailsActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/activities/TweetDetailsActivity.java
@@ -29,6 +29,9 @@ public class TweetDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.ic_launcher_twitter);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
         setContentView(R.layout.activity_tweet_details);
 
         ivProfileImage = (ImageView) findViewById(R.id.ivProfileImage);
